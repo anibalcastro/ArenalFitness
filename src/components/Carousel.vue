9785458b5a6de8fa6bd49442dd5e2577a1c0f872
anibalcastro <anibalcastro1515@gmail.com>
2023-12-21 10:09:35 -0600

    Carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <div>
+        <b-carousel
+          id="carousel-fade"
+          style="text-shadow: 0px 0px 2px #000"
+          fade
+          indicators
+          img-width="1024"
+          img-height="480"
+        >
+          <b-carousel-slide
+            caption="Arenal Fitness Gym"
+            img-src="https://picsum.photos/1024/480/?image=10"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="Clase Funcional"
+            img-src="https://picsum.photos/1024/480/?image=12"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="Gimnasio"
+            img-src="https://picsum.photos/1024/480/?image=22"
+          ></b-carousel-slide>
+        </b-carousel>
+      </div>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppCarousel",
+  data() {
+    return {
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    },
+  },
+};
+</script>
+
+<style>
+/* Your component-specific styles if needed */
+</style>
