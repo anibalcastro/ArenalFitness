4c81df7c5fb5ce1ec175507fbc6bd29d93e5e0ad
anibalcastro <anibalcastro1515@gmail.com>
2023-12-21 10:10:07 -0600

    App Arenal Fitness

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Home />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Sidebar />
+    <Carousel />
+  </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar.vue'
+import Carousel from '../components/Carousel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Carousel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
